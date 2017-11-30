cc2af3a8797562c3209accf7ce2eff9f948a0d2c
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2017-11-30 14:52:16 +0100

    Flex View

--- src/CountryPicker.js
+++ src/CountryPicker.js
@@ -306,7 +306,7 @@ export default class CountryPicker extends Component {
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
         <TouchableOpacity
           disabled={this.props.disabled}
           style={this.props.style}
